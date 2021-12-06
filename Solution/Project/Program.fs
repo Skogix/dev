f94f00be368cb02f5540690d26887753611c7dea
Emil Skogsund <skogix@gmail.com>
2021-12-06 15:45:58 +0100

    UPDATEALL:mån  6 dec 2021 15:45:56 CET:

--- Solution/Project/Program.fs
+++ Solution/Project/Program.fs
@@ -1,2 +1,18 @@
 open ClassLibrary1
+open System
+Console.Clear()
 Say.hello "Test"
+type Test =
+  | A
+  | B
+  | C
+  | D
+let huhu x = 
+  match x with
+  | A -> printfn "Test A"
+  | B -> printfn "Test A"
+  | C -> printfn "Test A"
+  | D -> printfn "Test A"
+huhu B
+let a = 0
+a
