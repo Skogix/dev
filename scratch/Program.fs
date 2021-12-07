84a20834df1ff1e08c75c28c328405420fd41512
Emil Skogsund <skogix@gmail.com>
2021-12-07 16:34:06 +0100

    UPDATEALL:tis  7 dec 2021 16:34:04 CET:

--- scratch/Program.fs
+++ scratch/Program.fs
@@ -2,23 +2,4 @@
 
 Console.Clear()
 
-module MonadCombinator =
-  let (|Yay|Nay|) = function
-    | Choice1Of2 y -> Yay y
-    | Choice2Of2 n -> Nay n
-  let yay x = Choice1Of2 x
-  let nay x = Choice2Of2 x 
-  let either yayFunc nayFunc input =
-    match input with
-    | Yay y -> yayFunc y
-    | Nay n -> nayFunc n
-  let bind f = either f nay
-open MonadCombinator
-let carbonate factor label i =
-  if i % factor = 0 then
-    yay label
-  else
-    nay i
-let connect f = function
-  | Yay y -> yay y
-  | Nay n -> f n
+printfn "Test"
