aec438c4308bb24525645908c9f87e8db4bc4594
Emil Skogsund <skogix@gmail.com>
2021-12-08 20:09:20 +0100

    UPDATEALL:ons  8 dec 2021 20:09:19 CET:

---
+++ exercism/fsharp/beer-song/BeerSong.fs
@@ -0,0 +1,17 @@
+﻿module BeerSong
+let bottlesOfBeer n = $"{n} bottles of beer on the wall, {n} bottles of beer."
+let takeOnDown = function
+    | x when x > 1 -> 
+        $"Take one down and pass it around, {x-1} bottles of beer on the wall."
+    | x when x = 1 -> 
+        $"Take one down and pass it around, no more bottles of beer on the wall"
+    | _ -> ""
+type Test =
+    | A of int
+    | B
+    | C
+let test x =
+    match x with
+    | A n -> ()
+let recite (startBottles: int) (takeDown: int): string list = 
+    []
