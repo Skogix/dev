0eb2382fc9cc811d9750313c09f7dbfbcc82f0f2
Emil Skogsund <skogix@gmail.com>
2022-02-17 12:14:26 +0100

    UPDATEALL:tor 17 feb 2022 12:14:25 CET:

---
+++ cppFromScratch/Main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <printf.h>
+void Print(const char* message);
+int main()
+{
+  //std::cout << "Hello World!" << std::endl;
+  Print("Test");
+  return 0;
+}
---
+++ cppFromScratch/Print.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void Print(const char* message){
+  std::cout << message << std::endl;
+}
